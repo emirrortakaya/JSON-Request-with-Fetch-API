8d2b535454022054c5993da46868e5ecc26e9fb3
Emir Ortakaya <113857017+emirrortakaya@users.noreply.github.com>
2022-10-05 23:13:57 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+        const newPost = 
+        {
+            age : 99,
+            name : "Your Name",
+            surname : "Your Age",
+            registered : "2022-07-05T11:51:05-03:00",
+        }
+        
+        document.querySelector("button").addEventListener("click",()=>{
+            fetch("http://localhost:8080/api/users/add",{
+                method : "POST",
+                body : JSON.stringify(newPost),
+                headers : {"Content-type":"application/json"}
+            })
+            .then(response=>response.json())
+            .then(json=>{
+                console.log(json)
+            })
+            .catch(err=>console.log(err))
+        })
